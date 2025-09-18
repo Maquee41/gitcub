4730008f2951d474aa5cc00181af0ed20f042ee8
Egor <96100120+Maquee41@users.noreply.github.com>
2025-09-18 11:43:56 +0300

    refactor(config): update aliases vite.config.ts
    Co-authored-by: Alexander Stolba <sashafootball2018@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,8 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
+alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
     },
   },
 })
